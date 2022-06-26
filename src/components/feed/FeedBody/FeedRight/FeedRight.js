d74c53ac1a9c767c3b1ca8c06a0ad21a94ea8bc1
Tangilur Rahman <tangilurrahaman@gmail.com>
2022-06-27 02:15:10 +0600

    feed-right done

--- src/components/feed/FeedBody/FeedRight/FeedRight.js
+++ src/components/feed/FeedBody/FeedRight/FeedRight.js
@@ -1,10 +1,19 @@
-import "./FeedRight.css"
+//own components
+import Hashtags from "./components/Hashtags/Hashtags";
+import Sponsors from "./components/Sponsors/Sponsors";
+import TrendingPage from "./components/TrendingPage/TrendingPage";
+
+import "./FeedRight.css";
 
 const FeedRight = () => {
 	return (
 		<>
 			{/* **** feed-right start **** */}
-			<div className="col-3 p-0 feed-right">feed right</div>
+			<div className="col-3 p-0 feed-right">
+				<Sponsors />
+				<TrendingPage/>
+				<Hashtags/>
+			</div>
 			{/* **** feed-right end **** */}
 		</>
 	);
